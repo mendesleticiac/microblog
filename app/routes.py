741f3a70e5e5d6a414546f1cfe664de6579ce32b
Leticia <cristinayang23900@gmail.com>
2026-09-01 14:47:16 -0300

    pagina login

--- app/routes.py
+++ app/routes.py
@@ -2,13 +2,19 @@ from app import app
 from flask import render_template
 
 @app.route('/')
-@app.route('/index/<nome>')
+@app.route('/index' , defaults={"nome":"usuário"})
+@app.route('/index/<nome>/<Profissao>/<Status>')
 
-def index(nome):
-    dados={"Profissao": "Professora","Status": "Casada"}
+def index(nome, Profissao, Status):
+
+    dados={"Profissao": Profissao ,"Status": Status}
     return render_template('index.html', nome=nome, dados=dados)
 
 
 @app.route ('/contato')
 def contato():
     return render_template('contato.html')
+
+@app.route ('/login')
+def login():
+    return render_template('login.html')
